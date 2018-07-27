76562540e13769b4723c014fe96df1d12d053eef
yourpropertyexpert <yourpropertyexpert@gmail.com>
2018-07-27 14:08:52 +0100

    Extended readmes

--- machineweb/pushreading.php
+++ machineweb/pushreading.php
@@ -4,6 +4,15 @@
 // There is complexity about using localhost for Mysql connections,
 // so for Docker on Mac (which I use), it's better to use host.docker.internal
 
+// You will note that I'm explicitly including the credentials here, rather than sharing
+// them with /site. This is to allow this page to use a database user with R/W access,
+// and the /site one to use a database user with Read-Only access.
+
+
+// In production, it is better to move the credentials out to the environment rather than 
+// having them in the code.
+
+
 $dbservername = "host.docker.internal";
 $dbdatabasename = "gardenlogger";
 $dbusername = "username";
