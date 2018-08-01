996f4c6ce60a6a8701a0c73abc062821dc40ec31
yourpropertyexpert <yourpropertyexpert@gmail.com>
2018-08-01 14:41:14 +0100

    Added human readable stuff to the index

---
+++ machineweb/index.php
@@ -0,0 +1,5 @@
+<h1>GardenLogger</h1>
+
+<p>If you are seeing this file, you are pointing a web browser at the "machineweb" directory.</p>
+
+<p>The page in this directory isn't really intended to be viewed by a human. Instead, it's for the Python code running on the Raspberry Pi to push data to</p>
