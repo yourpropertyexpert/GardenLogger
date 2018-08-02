100213d7cae06bf2335b5af1dc90864b6301c732
yourpropertyexpert <yourpropertyexpert@gmail.com>
2018-08-02 12:01:49 +0100

    Change to the web site to remove messages about a different site being needed for the Pi

--- web/index.php
+++ web/index.php
@@ -1,5 +1,3 @@
 <h1>GardenLogger</h1>
 
-<p>If you are seeing this file, you are pointing a web browser at the "machineweb" directory.</p>
-
-<p>The page in this directory isn't really intended to be viewed by a human. Instead, it's for the Python code running on the Raspberry Pi to push data to</p>
+<p>If you are seeing this page, then it means that your webserver is running</p>
